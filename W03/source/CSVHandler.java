b1e1400146cb1bb5bf130f46cc18e336d7b6a7ab
Justin Yang <justinyang28@googlemail.com>
2017-02-05 12:27:57 +0000

    First commit

---
+++ W03/source/CSVHandler.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CSVHandler {
+
+    private ArrayList<Record> al = new ArrayList<Record>();
+    private String line;
+    private String[] informationArray;
+
+    /*
+        This constructor obtains the specified file path and reads every line of that file. It then splits
+        line by commas and puts them into a string array. The string array is then passed into the Record class
+        where the comma-split words is turned into a record. The record is then added to an arraylist of records.
+    */
+    public CSVHandler(String path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            line = br.readLine();
+            while (line != null) {
+                    informationArray = line.split(","); //split the line by commas to obtain a string array
+                    Record aRecord = new Record(informationArray); //pass the array to the Record class
+                    al.add(aRecord); //add the Record to the array
+                    line = br.readLine(); //read the next line
+            }
+
+        } catch (IOException e) {
+            System.out.println("File not found error!");
+        }
+    }
+
+    //method which returns the ArrayList of records
+    public ArrayList<Record> getRecordArrayList() {
+        return al;
+    }
+}
---
+++ W03/source/Record.java
@@ -0,0 +1,121 @@
+public class Record {
+
+    //static variables where the column headers are stored in the csv
+    private static final int INVOICE_NUMBER_IN_CSV = 0;
+    private static final int STOCK_CODE_IN_CSV = 1;
+    private static final int DESCRIPTION_IN_CSV = 2;
+    private static final int QUANTITY_IN_CSV = 3;
+    private static final int INVOICE_DATE_IN_CSV = 4;
+    private static final int INVOICE_UNIT_PRICE_IN_CSV = 5;
+    private static final int CUSTOMER_ID_IN_CSV = 6;
+    private static final int COUNTRY_IN_CSV = 7;
+
+    private String invoiceNumber;
+    private String stockCode;
+    private String description;
+    private Integer quantity;
+    private String invoiceDate;
+    private Double unitPrice;
+    private Integer customerID;
+    private String country;
+    private boolean isCanceled = false;
+
+    //constructor for Record
+    public Record(String[] fieldArray) {
+        setInvoiceNumber(fieldArray[INVOICE_NUMBER_IN_CSV]);
+        setStockCode(fieldArray[STOCK_CODE_IN_CSV]);
+        setDescription(fieldArray[DESCRIPTION_IN_CSV]);
+        setQuantity(fieldArray[QUANTITY_IN_CSV]);
+        setInvoiceDate(fieldArray[INVOICE_DATE_IN_CSV]);
+        setUnitPrice(fieldArray[INVOICE_UNIT_PRICE_IN_CSV]);
+        setCustomerID(fieldArray[CUSTOMER_ID_IN_CSV]);
+        setCountry(fieldArray[COUNTRY_IN_CSV]);
+    }
+
+    //setters
+    private void setInvoiceNumber(String newInvoiceNumber) {
+        if (newInvoiceNumber.startsWith("C")) {
+            isCanceled = true;
+            invoiceNumber = newInvoiceNumber;
+        } else {
+            invoiceNumber = newInvoiceNumber;
+        }
+    }
+
+    private void setStockCode(String newStockCode) {
+        stockCode = newStockCode;
+    }
+
+    private void setDescription(String newDescription) {
+        description = newDescription;
+    }
+
+    private void setQuantity(String newQuantity) {
+        try {
+            quantity = Integer.parseInt(newQuantity);
+        } catch (NumberFormatException e) {
+            quantity = null;
+        }
+    }
+
+    private void setInvoiceDate(String newInvoiceDate) {
+        invoiceDate = newInvoiceDate;
+    }
+
+    private void setUnitPrice(String newUnitPrice) {
+        try {
+            unitPrice = Double.parseDouble(newUnitPrice);
+        } catch (NumberFormatException e) {
+            unitPrice = null;
+        }
+    }
+
+    private void setCustomerID(String newCustomerID) {
+        try {
+            customerID = Integer.parseInt(newCustomerID);
+        } catch (NumberFormatException e) {
+            customerID = null;
+        }
+    }
+
+    private void setCountry(String newCountry) {
+        country = newCountry;
+    }
+
+    //getters
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public boolean getIsCanceled() {
+        return isCanceled;
+    }
+}
