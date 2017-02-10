e519bbc7d82f59381c5b5542e628df14557b523c
Justin Yang <justinyang28@googlemail.com>
2017-02-10 17:33:07 +0000

    made it more efficient

--- W03-Practical/source/TextWriter.java
+++ W03-Practical/source/TextWriter.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class TextWriter {
 
     /*
-        This class takes the ArrayList of Records and writes a .txt fle formatted as per specifiication.
-    */
+     *This class takes the ArrayList of Records and writes a .txt fle formatted as per specifiication.
+     */
     public TextWriter(ArrayList<Record> al, String exportDestination) {
         //remove the first line in the arrayList as that contains the column headers
         al.remove(0);
@@ -21,7 +21,7 @@ public class TextWriter {
         String target = al.get(0).getInvoiceNumber();
 
         //aray to store and calcuate total invoice prices
-        ArrayList<Double> priceArrayList = new ArrayList<Double>();
+        Double invoiceNoTotal = 0.0;
 
         //create buffered writer to write to file
         try {
@@ -38,35 +38,35 @@ public class TextWriter {
                     writeInvoiceDetails(al, j, exportDestination, out);
 
                     //add the price of the invoice line to an arraylist that stores the running total for this invoice number
-                    priceArrayList.add(al.get(j).getUnitPrice() * al.get(j).getQuantity());
+                    invoiceNoTotal += al.get(j).getUnitPrice() * al.get(j).getQuantity();
 
                     //set the target invoice number as the current invoice number
                     target = al.get(j).getInvoiceNumber();
 
                     //check if this invoice number is the last record in the array
-                    checkForLast(al, j, priceArrayList, hmap, exportDestination, out);
+                    checkForLast(al, j, invoiceNoTotal, hmap, exportDestination, out);
 
                 //if the target invoice number and the invoice number in the next position of the Record Arraylist do not match,
                 //this is a new invoice number since the data is assumed to be already sorted
                 } else {
 
                     //display the total of the previous prices of the invoice together
-                    writeTotalCost(calculateTotalCost(priceArrayList, al.get(j - 1).getInvoiceNumber(), hmap), exportDestination, out);
+                    writeTotalCost(calculateTotalCost(invoiceNoTotal, al.get(j - 1).getInvoiceNumber(), hmap), exportDestination, out);
 
                     //clear the price arraylist
-                    priceArrayList.clear();
+                    invoiceNoTotal = 0.0;
 
                     //write the new invoice details to a file
                     writeInvoiceDetails(al, j, exportDestination, out);
 
                     //add the price of the invoice line to an arraylist that stores the running total for this invoice number
-                    priceArrayList.add(al.get(j).getUnitPrice() * al.get(j).getQuantity());
+                    invoiceNoTotal += al.get(j).getUnitPrice() * al.get(j).getQuantity();
 
                     //set the target invoice number as the current invoice number
                     target = al.get(j).getInvoiceNumber();
 
                     //check if this invoice number is the last record in the array
-                    checkForLast(al, j, priceArrayList, hmap, exportDestination, out);
+                    checkForLast(al, j, invoiceNoTotal, hmap, exportDestination, out);
                 }
             }
 
@@ -82,27 +82,18 @@ public class TextWriter {
     }
 
     /*
-        method which calculates the total cost of an invoice since the priceArrayList stores the
-        cost for each line (i.e unit price * quantity for that particular invoice line).
-        The total cost for that invoice is mapped to a hashmap using its invoice number as a key
-        This method returns the total cost of the invoice so that it can be wrritten into the txt file
-    */
-    private double calculateTotalCost(ArrayList<Double> priceArrayList, String invoiceNumber, HashMap<String, Double> hmap) {
-        double total = 0;
-
-        for (double price : priceArrayList) {
-            total = total + price;
-        }
-
+     * Method which maps the total cost associated with a particular invoice number to the total cost of that invoice number
+     */
+    private double calculateTotalCost(Double invoiceNoTotal, String invoiceNumber, HashMap<String, Double> hmap) {
         //inserting the total cost into a hashmap using the invoice number as a key
-        hmap.put(invoiceNumber, total);
+        hmap.put(invoiceNumber, invoiceNoTotal);
 
-        return total;
+        return invoiceNoTotal;
     }
 
     /*
-        method which writes the specified format of the otuput file as specified in the specification
-    */
+     * Method which writes the specified format of the otuput file as specified in the specification
+     */
     private void writeInvoiceDetails(ArrayList<Record> al, int j, String exportDestination, PrintWriter out) {
         out.println("Invoice Number: " + al.get(j).getInvoiceNumber());
         out.println("Stock Code: " + al.get(j).getStockCode());
@@ -112,9 +103,8 @@ public class TextWriter {
     }
 
     /*
-        method which writes to total cost of the invoice to the output file and adds in
-        a new line to separate the different invoices
-    */
+     * method which writes to total cost of the invoice to the output file and adds in a new line to separate the different invoices
+     */
     private void writeTotalCost(double totalCost, String exportDestination, PrintWriter out) {
         out.println("Total Price: " + totalCost);
         out.println();
@@ -156,11 +146,11 @@ public class TextWriter {
     }
 
     /*
-        method which checks if the current invoice number is the last record in the arrayList
-    */
-    private void checkForLast(ArrayList<Record> al, int j, ArrayList<Double> priceArrayList, HashMap<String, Double> hmap, String exportDestination, PrintWriter out) {
+     * Method which checks if the current invoice number is the last record in the arrayList
+     */
+    private void checkForLast(ArrayList<Record> al, int j, Double invoiceNoTotal, HashMap<String, Double> hmap, String exportDestination, PrintWriter out) {
         if (j == al.size() - 1) {
-            writeTotalCost(calculateTotalCost(priceArrayList, al.get(j).getInvoiceNumber(), hmap), exportDestination, out);
+            writeTotalCost(calculateTotalCost(invoiceNoTotal, al.get(j).getInvoiceNumber(), hmap), exportDestination, out);
         }
     }
 
