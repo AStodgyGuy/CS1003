3a643c3f0da2551d52e628a7b3d4e26384fd2a78
Justin Yang <justinyang28@googlemail.com>
2017-02-10 18:27:00 +0000

    added exception

--- W03-Practical/source/W03Practical.java
+++ W03-Practical/source/W03Practical.java
@@ -23,8 +23,8 @@ public class W03Practical {
 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Usage: java W03Practical <input_file> <output_file>");
-        } //catch (IndexOutOfBoundsException e) {
-           // System.out.println("Usage: java W03Practical <input_file> <output_file>");
-       // }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Usage: java W03Practical <input_file> <output_file>");
+        }
     }
 }
