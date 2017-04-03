fe411a0dd4df37d8779fe4809420a445d6662962
Justin Yang <justinyang28@googlemail.com>
2017-04-03 12:56:46 +0100

    first commit for week 9

---
+++ W09-Practical/src/Describe.java
@@ -0,0 +1,12 @@
+import javax.json.JsonReader;
+import javax.json.JsonObject;
+import javacx.json.JsonArray;
+
+
+public class Describe {
+    public static void main(String[] args) {
+        //obtain the json file
+        //process the json file
+        //display the json file summary to the user
+    }
+}
